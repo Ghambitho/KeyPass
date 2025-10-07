05afd3596f3d799b49f5a4340142639b366a3752
Ghambitho D. Hack <jhonbelandria98@gmail.com>
2025-10-07 18:56:25 -0300

    V9

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Punto de entrada principal para KeyPass API
+"""
+
+if __name__ == "__main__":
+    import uvicorn
+    from api.api import app
+    
+    # Para desarrollo local
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+else:
+    # Para producción (importado por Render)
+    from api.api import app
